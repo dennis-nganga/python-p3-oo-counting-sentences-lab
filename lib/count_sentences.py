2deb5c6f9e99eff10f7fb4e90c8daa3ae151c416
dennis-nganga <denniskimani416@gmail.com>
2023-05-23 22:52:28 +0300

    counting sentences

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value):
+        self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        sentences = self.value.split(".")
+        return len(sentences)
+
+    def is_string(self):
+        if not isinstance(self.value, str):
+            return "The value must be a string."
+        return ""
+
